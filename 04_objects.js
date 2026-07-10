dfbd5617d7dd12ce094e25d1cbb6981ddd450a52
Sumit <sumitkum4747@gmail.com>
2026-07-10 22:26:49 +0530

    Add files via upload

---
+++ 04_objects.js
@@ -0,0 +1,20 @@
+const tinderUser = new Object()
+
+tinderUser.id = "123abc";
+tinderUser.name = "Sumit";
+tinderUser.isLoggedIn = false
+
+console.log(tinderUser);
+
+
+const regularUser = {
+    email: "sumit@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Sumit",
+            lastname : "Yadav",
+        }
+    }
+}
+
+console.log(regularUser);
